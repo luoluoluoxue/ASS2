900c781045fb2cf7eb9933da5546a7faaa739ff7
1Huihuihui1 <118098674+1Huihuihui1@users.noreply.github.com>
2023-05-03 23:36:07 +0800

    Update update-customer.php

--- backend/user/update-customer.php
+++ backend/user/update-customer.php
@@ -1,64 +1,56 @@
 <?php
 
-//这个有问题啊啊啊啊啊啊啊啊啊啊啊
-//让我改啊啊啊啊啊啊啊啊啊啊啊
-//那两个没问题（应该）
-//为什么有问题啊啊啊啊啊啊啊啊啊啊啊啊烦死我了啊啊啊啊啊啊啊啊啊啊啊啊
-
-
-namespace app\controller;
-
-use pdo\Pdo;
-use think\facade\Config;
-
-class update_customer
-{
-
-    /*更新顾客信息*/
-    public function index()
-    {
-        
-                 // 创建 PDO 连接
-                //open database by PDO
-        $dbms='sqlite';     //DBMS type
-        $host=''; //Host name
-        $dbName='数据库名字叫啥.db';    //database name
-        $user='';      //database user
-        $pass='';          //database password
-        $dsn="$dbms:$dbName";
-
-        try {
-            $con = new PDO($dsn, $user, $pass);
+//update-customer
+    /**更新顾客信息*/
+  
+        // 创建 PDO 连接
+         try {
+            $pdo = new PDO("mysql:host=localhost; dbname=testcostumer","root","");
         } catch (PDOException $e) {
             die ("Error!: " . $e->getMessage() . "<br/>");
         }
 
+        $request_body = file_get_contents('php://input');
+        $data = json_decode($request_body, true);
 
         // 获取请求参数
-        $id = $_POST['id'];
-        $name = $_POST['name'];
-        $address = $_POST['address'];
-        $payment = $_POST['payment'];
+        $id = isset($data['id'])?$data['id']:'';
+        $name = isset($data['name'])?$data['name']:'';
+        $address = isset($data['address'])?$data['address']:'';
+        $payment = isset($data['payment'])?$data['payment']:'';
 
-        // 构造 SQL 语句，更新 tb_customer_info 表中 id 为 $id 的记录
-        $sql = "SELECT * FROM tb_customer_info WHERE  id=? and user_nickname=? and default_address=? and default_payment_type=?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$id,$name, $address, $payment]);
-        $order_list = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
-        if (!empty($order_list)) {
-            // 更新成功
-            $result['code']=200;
-            $result['message']="success";
-            echo json_encode($result);
-        } else {
+        if(!empty($id) && !empty($name) && !empty($address) && !empty($payment)){
+            // 构造 SQL 语句，更新 tb_customer_info 表中 id 为 $id 的记录
+            $sql = "SELECT * FROM tb_customer_info WHERE  id=?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$id]);
+            $customer_info = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            if($customer_info){
+                // 构造 SQL 语句，更新 tb_customer_info 表中 id 为 $id 的记录
+                $sql = "UPDATE tb_customer_info SET user_nickname=?, default_address=?, default_payment_type=? WHERE id=?";
+                $stmt = $pdo->prepare($sql);
+                $res = $stmt->execute([$name, $address, $payment, $id]);
+                if ($res!=false) {
+                    // 更新成功
+                    $result['code']=200;
+                    $result['message']="success";
+                    echo json_encode($result);
+                } else {
+                    // 更新失败
+                    $result['code']=404;
+                    $result['message']="fail";
+                    echo json_encode($result);
+                }
+            }else{
+                // 更新失败
+                $result['code']=404;
+                $result['message']="fail";
+                echo json_encode($result);
+            }
+        }else{
             // 更新失败
             $result['code']=404;
             $result['message']="fail";
             echo json_encode($result);
         }
-
-
-    }
-
-}
+?>
